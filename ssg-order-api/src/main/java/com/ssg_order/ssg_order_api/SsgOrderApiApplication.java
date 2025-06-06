b85968721e0fde42057f09929242e522bed3cb1e
Sujin Kim <sj366345@gmail.com>
2025-06-06 12:27:41 +0900

    project created

---
+++ ssg-order-api/src/main/java/com/ssg_order/ssg_order_api/SsgOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssg_order.ssg_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgOrderApiApplication.class, args);
+	}
+
+}
---
+++ ssg-order-api/src/test/java/com/ssg_order/ssg_order_api/SsgOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg_order.ssg_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
